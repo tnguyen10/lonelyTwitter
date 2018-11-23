31a9f4fc4012418bb4c807e2ec614ea8ab9fb745
= <tnguyen1@ualberta.ca>
2018-11-22 19:23:37 -0700

    added some camera functionality

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -4,28 +4,40 @@
 
 package ca.ualberta.cs.lonelytwitter;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import static ca.ualberta.cs.lonelytwitter.PermissionRequest.verifyPermission;
 
 /**
  * Represents a LonelyTwitterActivity
@@ -40,6 +52,7 @@ import com.google.gson.reflect.TypeToken;
 
 public class LonelyTwitterActivity extends Activity {
 
+    Uri imageFileUri;
     private static final String FILENAME = "file.sav";
     private EditText bodyText;
     private ListView oldTweetsList;
@@ -84,6 +97,14 @@ public class LonelyTwitterActivity extends Activity {
                 adapter.notifyDataSetChanged();
             }
         });
+
+        ImageButton button = (ImageButton) findViewById(R.id.TakeAPhoto);
+        View.OnClickListener listener = new View.OnClickListener() {
+            public void onClick(View v){
+                takeAPhoto();
+            }
+        };
+        button.setOnClickListener(listener);
     }
 
     /**
@@ -99,6 +120,53 @@ public class LonelyTwitterActivity extends Activity {
         adapter = new ArrayAdapter<Tweet>(this,
                 R.layout.list_item, tweets);
         oldTweetsList.setAdapter(adapter);
+    }
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    public void takeAPhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        String folder = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download";
+        File folderF = new File(folder);
+        if (!folderF.exists()) {
+            folderF.mkdir();
+        }
+
+        //if(Build.VERSION.SDK_INT>=16) {
+        try {
+            Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
+            m.invoke(null);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //}
+
+        verifyPermission(this);
+
+        String imageFilePath = folder + "/" + String.valueOf(System.currentTimeMillis()) + "jpg";
+
+        File imageFile = new File(folder,"imagetest.jpg");
+        imageFileUri = Uri.fromFile(imageFile);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            TextView tv = (TextView) findViewById(R.id.status);
+            if (resultCode == RESULT_OK) {
+                tv.setText("Photo OK!");
+                ImageButton button = (ImageButton) findViewById(R.id.TakeAPhoto);
+                button.setImageDrawable(Drawable.createFromPath(imageFileUri.getPath()));
+            } else if (resultCode == RESULT_CANCELED) {
+                tv.setText("Photo canceled");
+            } else {
+                tv.setText("Not sure what happened!" + resultCode);
+            }
+        }
     }
 
     /**
